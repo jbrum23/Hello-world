026ca0d599ec6c92ab7e07322c15c1836863cfe3
jbrum23 <john.brum@student.fairfield.edu>
2026-01-28 20:30:48 +0000

    Functions

---
+++ Function1 Jan 28.py
@@ -0,0 +1,14 @@
+# Greet the user by name, ensuring proper capitalization and removing extra spaces
+
+def hello(to):
+    print("Hello," , to)
+
+# output greeting
+name = input("What is your name? ")
+hello(name)
+
+# Output without passing the expected arguments
+hello()
+
+def hello(to):
+    print("Hello," , to)
